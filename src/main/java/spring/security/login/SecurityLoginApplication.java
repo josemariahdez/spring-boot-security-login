72e12891d88f7732d1d6190dc3d4dd5a56feac0f
Chema Hdez <jomher1986@gmail.com>
2019-01-19 19:13:38 +0100

    Created project

---
+++ src/main/java/spring/security/login/SecurityLoginApplication.java
@@ -0,0 +1,14 @@
+package spring.security.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityLoginApplication.class, args);
+	}
+
+}
+
